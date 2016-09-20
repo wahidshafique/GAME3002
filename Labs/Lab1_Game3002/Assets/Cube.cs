15915b9d62aacf5a3cd41e676cb94d946f94107f
Wahid Shafique <wahidshafique@hotmail.com>
2016-09-20 09:13:48 -0400

    Added base files

--- Labs/Lab1_Game3002/Assets/Cube.cs
+++ Labs/Lab1_Game3002/Assets/Cube.cs
@@ -2,65 +2,88 @@
 using System.Collections;
 
 public class Cube : MonoBehaviour {
-    Rigidbody rb; 
-    public float m_acceleration;
+    Rigidbody[] rbs;
+    public float m_accelerationPorsche;
+    public float m_accelerationLaFerrari;
+
     public float m_initVel = 0;
     public float m_finalVel = 100;
-    public float m_timeInSec = 2.2f;
-    float maxDist = 4;
+
+    float m_timeInSecPorsche = 2.2f;
+    float m_timeInSecLaFerrari = 2.4f;
+
+    float distanceTravelledPorsche = 0;
+    float distanceTravelledLaFerrari = 0;
+
+    Vector3 previous;
+
     bool toggle = false;
-    
-	// Use this for initialization
-	void Start () {
-        rb = this.gameObject.GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown("space"))
-        {
+    GameObject[] cubes;
+
+    // Use this for initialization
+    void Start() {
+        cubes = GameObject.FindGameObjectsWithTag("Cube");
+        rbs = new Rigidbody[cubes.Length];
+        for (int i = 0; i < cubes.Length; i++)
+            rbs[i] = cubes[i].GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+
+    void Update() {
+        
+        if (Input.GetKeyDown("space")) {
+            
             toggle = true;
-        }
-        if (toggle)
-        {
-            startSim();
         }
     }
 
-    void startSim()
-    {
-        print("start");
-        m_acceleration = calcAccel(m_initVel, m_finalVel, m_timeInSec);
-        
+    void FixedUpdate() {
+
+        if (toggle) {
+            Sim();
+        };
+    }
+
+    void Sim() {
+        m_accelerationPorsche = calcAccel(m_initVel, kmphToMps(m_finalVel), m_timeInSecPorsche, "Porche");
+        m_accelerationLaFerrari = calcAccel(m_initVel, kmphToMps(m_finalVel), m_timeInSecLaFerrari, "LaFerrari");
         addForce();
     }
-    float calcAccel(float initialVel, float finalVel, float time) {
-        finalVel = initialVel + (m_acceleration * time);
+    void calcVelocity(GameObject vehicle) {
+        float velocity = ((vehicle.transform.position - previous).magnitude) / Time.fixedDeltaTime;
+        previous = vehicle.transform.position;
+        print("velocity of Porche is " + velocity);
+        //now apply it to test vehicle [2]
+        rbs[2].velocity = new Vector3(-velocity, 0, 0);
+    }
+    float calcAccel(float initialVel, float finalVel, float time, string carType) {
+        if (carType == "Porche")
+            time = m_timeInSecPorsche;
+        else time = m_timeInSecLaFerrari;
         float a = (finalVel - initialVel) / time;
         print(a);
         return a;
     }
-    
-    float calcVel(float initalVel, float finalVel, float accel, float time)
-    {
-        finalVel = initalVel + (accel * Time.deltaTime);
-        return finalVel;
-    }
-    //convert vel based on acceleration
-    void Move()
-    {
-        if (this.transform.position.x <= maxDist)
-        {
-            Vector3.Lerp(transform.position, transform.position += new Vector3(-1, 0, 0), m_acceleration);
-        }
-    }
 
-    void addForce()
-    {
-        rb.AddForce(Vector3.left * rb.mass * m_acceleration);
+    void addForce() {
+        //if (distanceTravelledLaFerrari >= 2404 && distanceTravelledPorsche >= 2404) {
+        //    toggle = false;
+        //    rbs[0].velocity = new Vector3(0, 0, 0);
+        //    rbs[1].velocity = new Vector3(0, 0, 0);
+        //    print("sim over");
+        //    return;
+        //}
+        //so this adds force based on the accel formula
+
+        print("adding force to both cars");
+        rbs[0].AddForce(Vector3.left * rbs[0].mass * m_accelerationPorsche);
+        rbs[1].AddForce(Vector3.left * rbs[1].mass * m_accelerationLaFerrari);
+
+        //for testing, this adds force using velocity to the TEST vehicle (it uses the Porsche as a test case)
+        calcVelocity(rbs[0].gameObject);
+        previous = rbs[0].transform.position;
     }
-    float kmphToMps(float kmph)
-    {
+    float kmphToMps(float kmph) {
         return kmph = (kmph * 5) / 18;
     }
 }
---
+++ Labs/Lab1_Game3002/Assets/Length.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Length : MonoBehaviour {
+    public float metersOfTrack = 402;
+	// Use this for initialization
+	void Start () {
+        MeshCollider mColl = GetComponent<MeshCollider>();
+        transform.localScale = new Vector3(metersOfTrack, 1, 1);
+        GameObject[] cubeArr = GameObject.FindGameObjectsWithTag("Cube");
+        for (int i = 0; i < cubeArr.Length; i++) {
+            cubeArr[i].transform.position = new Vector3((mColl.bounds.size.x / 2) - 8, mColl.bounds.size.y + 1, mColl.bounds.size.z - (6 + i * 3));
+        }
+        //transform.position = new Vector3 ((meters * GameObject.Find("Cube").transform.position.x) + meters, 0, 0);
+	}
+
+}
