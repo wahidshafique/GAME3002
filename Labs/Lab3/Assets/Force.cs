d60d933299e1504cc12f55dc399a3c879232c88e
Wahid Shafique <wahidshafique@hotmail.com>
2016-09-22 21:57:37 -0400

    add lab 3

---
+++ Labs/Lab3/Assets/Force.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Force : MonoBehaviour {
+    GameObject refer;
+    Rigidbody rb;
+    void Awake() {
+        refer = GameObject.FindGameObjectWithTag("Ref");
+        rb = this.GetComponent<Rigidbody>();
+    }
+	void Start () {
+        float force = Mathf.Sqrt(2 * refer.transform.position.y * 9.8f);
+        //impulse is essentially doing mass*distance/time (Force per frame)
+        rb.AddForce(0, force, 0, ForceMode.Impulse);
+    }
+}
