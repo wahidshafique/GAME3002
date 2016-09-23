ce01491b45469aa08233df6e14e482821a3fccbe
Wahid Shafique <wahidshafique@hotmail.com>
2016-09-22 21:58:11 -0400

    complete

---
+++ Class/Class_Lab3/Assets/Lab3.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lab3 : MonoBehaviour {
+    Rigidbody m_rb;
+    public float m_totalTime, m_initVel = 0.0f, m_totalDisplace;
+    private float m_acceleration = 0.0f;
+    private Vector3 m_netForce = Vector3.up;
+    private bool m_isRunning = false;
+
+    void Start() {
+        m_rb = this.GetComponent<Rigidbody>();
+        m_acceleration = (CalculateAcceleration(m_totalDisplace, m_initVel, m_totalTime));
+        m_netForce *= (m_acceleration * m_rb.mass) + (9.8f * m_rb.mass);
+    }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("toggle");
+            m_isRunning = !m_isRunning;
+        } 
+    }
+    void FixedUpdate() {
+        if (m_isRunning) {
+            m_rb.AddForce(m_netForce);
+        }
+    }
+    public float CalculateAcceleration(float displacement, float initialVelocity, float totalTime) {
+        return (totalTime > Mathf.Epsilon ? ((displacement - (initialVelocity * totalTime)) * 20f) / (totalTime * totalTime) : 0.0f);
+    }
+}
