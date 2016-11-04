b43ddc81184b9d1850700cc1382e6f12df4bea34
Wahid Shafique <wahidshafique@hotmail.com>
2016-11-03 21:39:17 -0400

    init a1

---
+++ A1/Midterm_Wahid_Shafique/Assets/Scripts/ImpulseTask1.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImpulseTask1 : MonoBehaviour {
+    public float m_time = 3;
+    public Transform m_referenceTransform;
+
+    private Rigidbody m_rb;
+    private Vector3 m_force = Vector3.zero;
+    private Vector3 m_desiredDisplacement = Vector3.zero;
+    private bool launched = false;
+    private float triggerPoint;
+    private float triggerVelocity;
+
+    public Text idealForce;
+    public Text actualForce;
+    public Slider powerSlider;
+    void Start() {
+        powerSlider.onValueChanged.AddListener(delegate { valueChangeChecker(); });
+        m_rb = GetComponent<Rigidbody>();
+        triggerPoint = GameObject.FindGameObjectWithTag("TriggerPoint").transform.position.x;
+    }
+    void valueChangeChecker() {
+        powerSlider.GetComponentInChildren<Text>().text = "Power Level: " + powerSlider.value;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && !launched) {
+            m_rb.AddForce((float)-powerSlider.value, 0, 0, ForceMode.Impulse);
+        }
+        if (Mathf.Round(this.transform.position.x) == Mathf.Round(triggerPoint) && !launched) {
+            actualForce.text = "X Force Actual : " + m_rb.velocity.x.ToString();
+            triggerVelocity = m_rb.velocity.x;
+            m_rb.velocity = Vector3.zero;
+            calculateOnTrigger();
+            launched = true;
+        }
+    }
+
+    void calculateOnTrigger() {//once you hit the marker, then launch
+        Vector3 ourPos = GetOffsetPosition(this.transform, true);
+        Vector3 refPos = GetOffsetPosition(m_referenceTransform, false);
+        m_desiredDisplacement = refPos - ourPos;
+        LaunchProjectile();
+    }
+
+    Vector3 GetOffsetPosition(Transform refTransform, bool isBottom) {
+        return isBottom ? refTransform.position - (new Vector3(0.0f, refTransform.localScale.y, 0.0f) * 0.5f) : refTransform.position + (new Vector3(0.0f, refTransform.localScale.y, 0.0f) * 0.5f);
+    }
+
+    void LaunchProjectile() {
+        m_force.y = CalculateYImpulse(m_desiredDisplacement.y, m_time);
+        print("fixed Y force to reach goal was: " + m_force.y);
+        idealForce.text = "X Force Ideal: " + ((m_desiredDisplacement.x / m_time) * m_rb.mass).ToString();
+        m_force.x = triggerVelocity * m_rb.mass;
+        //m_force.x = (m_desiredDisplacement.x / m_time) * m_rb.mass;
+        m_rb.AddForce(m_force / Time.fixedDeltaTime);
+        m_force = Vector3.zero;
+    }
+
+    float CalculateYImpulse(float displacement, float time) {
+        float velocity = (displacement - (0.5f * Physics.gravity.y * (time * time))) / (time);
+        return velocity * m_rb.mass;
+    }
+}
--- Labs/Lab4/Assets/Lab4.cs
+++ Labs/Lab4/Assets/Lab4.cs
@@ -2,41 +2,63 @@
 using System.Collections;
 
 public class Lab4 : MonoBehaviour {
-    [Tooltip("The time it will take to go on top of the wall. Half the time it takes to go to the target.")]
-    private float m_time;
-    private Vector3 m_displacement = Vector3.zero;
+
+    [Tooltip("The time it will take to go ovet the wall. Half the time it takes to go to the target.")]
+    public float m_time;
+
+    [Tooltip("Reference Object Transform")]
+    public Transform m_referenceTransform;
+    public float m_acceleration;
+
+    // Reference to rigidbody.
     private Rigidbody m_rb;
     private Vector3 m_force = Vector3.zero;
+    private Vector3 m_desiredDisplacement = Vector3.zero;
 
-    private Transform marker;
-    private Transform wall;
-
+    // Use this for initialization
     void Start () {
-        marker = GameObject.FindGameObjectWithTag("Marker").transform;
-        wall = GameObject.FindGameObjectWithTag("Wall").transform;
         m_rb = GetComponent<Rigidbody>();
-        m_displacement = marker.position - this.transform.position;
-        m_time = 5;
+        Vector3 ourPos = GetOffsetPosition(this.transform, true);
+        Vector3 refPos = GetOffsetPosition(m_referenceTransform, false);
+        m_desiredDisplacement = refPos - ourPos;
+
     }
+
+    Vector3 GetOffsetPosition(Transform refTransform, bool isBottom)
+    {
+        return isBottom ? refTransform.position - (new Vector3(0.0f, refTransform.localScale.y, 0.0f) * 0.5f) : refTransform.position + (new Vector3(0.0f, refTransform.localScale.y, 0.0f) * 0.5f);
+    }
+
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             LaunchProjectile();
         }
+
+        float axis = Input.GetAxis("Horizontal");
+        //check if there is movement on the horzintal axis
+        if (Mathf.Abs(axis) > Mathf.Epsilon)
+        {
+            //clamp the value to be either 1 or -1 we don't want fractions
+            axis = axis > Mathf.Epsilon ? 1.0f : -1.0f;
+            //right is the x axis, if you are moving on the z axis use forward
+            Vector3 force = m_acceleration * m_rb.mass * this.transform.right * axis;
+            m_rb.AddForce(force);
+        }
     }
 
     void LaunchProjectile()
     {
-        m_force.y = CalculateYImpulse(m_displacement.y, m_time);
-        m_force.x = (m_displacement.x / m_time) * m_rb.mass;
+        m_force.y = CalculateYImpulse(m_desiredDisplacement.y, m_time);
+        m_force.x = (m_desiredDisplacement.x / m_time) * m_rb.mass;
         m_rb.AddForce(m_force / Time.fixedDeltaTime);
         m_force = Vector3.zero;
     }
 
     float CalculateYImpulse(float displacement, float time)
     {
-        float velocity = time > Mathf.Epsilon  ? (displacement - 0.5f * (Physics.gravity.y) * (time * time) + displacement) / (time) : 0.0f;
+        float velocity = ( displacement - (0.5f * Physics.gravity.y * (time * time) ) ) / (time);
         //since we are starting at rest, the difference in velocity is the velocity we calculated
         return velocity * m_rb.mass;
     }
